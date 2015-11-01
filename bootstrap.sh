b5a963b241424011886c03a6caab60fe7af633e1
Nadeem Shabir <nadeem.shabir@talis.com>
2015-11-01 17:40:38 +0000

    added simple bootstrap.sh
    this is used to install dependencies, for the time being just vim
    plugins, will update this with dotbot later, for right now this works.

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ~/.vim
+git clone https://github.com/rking/ag.vim.git
+git clone https://github.com/kien/ctrlp.vim.git 
+git clone https://github.com/mattn/gist-vim.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/moll/vim-node.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/SirVer/ultisnips.git
+git clone https://github.com/tpope/vim-abolish.git
+git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/easymotion/vim-easymotion.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/rodjek/vim-puppet.git
+git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git
+git clone https://github.com/mattn/webapi-vim.git
+
