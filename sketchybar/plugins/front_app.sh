36cd38ec15767b17093765667ce96256f1a91061
Nadeem Shabir <nadeem.shabir+kiyanwang@gmail.com>
2026-04-04 14:44:05 +0100

    feat: show app icon in front app pill on sketchybar

--- sketchybar/plugins/front_app.sh
+++ sketchybar/plugins/front_app.sh
@@ -6,5 +6,7 @@
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  source "$CONFIG_DIR/plugins/icon_map.sh"
+  __icon_map "$INFO"
+  sketchybar --set "$NAME" label="$INFO" icon="$icon_result"
 fi
